f0673e4f0cbb76ad5322d4d32cb8fff1dfd51357
Carlos <bowyercarlos@gmail.com>
2024-07-28 22:16:38 -0400

    moved cors from app to server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const { PORT = 5001 } = process.env;
+const cors = require("cors");  
 
 const app = require("./app");
 const knex = require("./db/connection");
+
+app.use(cors()); 
 
 const listener = () => console.log(`Listening on Port ${PORT}!`);
 
